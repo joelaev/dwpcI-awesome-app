f4741fabacd9d84a43f3e0d60756283323f24ca2
Joel Escobar <160166118+joelaev@users.noreply.github.com>
2024-11-12 03:40:57 +0000

    📚Feat: Middleware

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ import http from 'http';
 import express from 'express';
 // Creando la instancia de express que basicamente es un middleware
 const app = express();
+// Registrando el primer middleware
+app.use((req, res, next)=>{
+    console.log("📢 Middleware #1");
+   // Se invoca al siguiente middlware
+   next();
+});
+// Registrando el segundo middleware
+app.use((req, res, next)=>{
+  console.log("📢 Middleware #2");
+  // Se invoca al siguiente middlware
+  next();
+});
 
 // Asignando el middleware de express al servidor http
 const server = http.createServer(app);
