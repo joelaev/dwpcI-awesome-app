0a4a14eba328d1320849ae27169bd5318b74c178
Joel Escobar <160166118+joelaev@users.noreply.github.com>
2024-11-13 03:27:35 +0000

    Feat: rutas middleware montado en una ruta

--- app.js
+++ app.js
@@ -2,17 +2,15 @@
 import http from 'http';
 // Importando Express
 import express from 'express';
-// Creando la instancia de express que basicamente es un middleware
+
+// Creando la instancia de express
+// que basicamente es un middleware
 const app = express();
-// Registrando el primer middleware
-app.use((req, res, next)=>{
-    console.log("📢 Middleware #1");
-   // Se invoca al siguiente middlware
-   next();
-});
-// Registrando el segundo middleware
-app.use((req, res, next)=>{
-  console.log("📢 Middleware #2");
+
+// Ruta Raíz
+// GET /
+app.use((req, res)=>{
+  console.log("📢 Sirviendo la ruta '/'");
   // Se contesta al server
   res.send(`
     <h1>Welcome to Express Js</h1>
@@ -20,13 +18,22 @@ app.use((req, res, next)=>{
   `);
 });
 
-// Asignando el middleware de express al servidor http
-const server = http.createServer(app);
+// Ruta about
+// GET /about
+app.use('/about',(req, res)=>{
+  console.log("📢 Sirviendo la ruta '/about'");
+  // Se contesta al server
+  res.send(`
+    <h1>🪄 About...</h1>
+    <p>App for Fullstack Web Dev Course I!</p>
+  `);
+});
 
 // Definiendo puertos
 const port = 3000;
 const ip = "0.0.0.0"
+
 // Arrancando el servidor
-  app.listen(port, ip, () => {
-    console.log(`🤖 Sirviendo en http://localhost:${port}`);
+app.listen(port, ip, () => {
+  console.log(`🤖 Sirviendo en http://localhost:${port}`);
 });
