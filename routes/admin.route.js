8e8c099bce5e82768e9f8499cfe7f42055a5f1be
Joel Escobar <160166118+joelaev@users.noreply.github.com>
2024-12-09 00:15:36 +0000

    🌲Feat: Patron MVC Se crea estructurado MVC

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,33 +1,16 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-// Importando el gestor de rutas
-import path from 'path';
-import { ROOT_DIR } from '../helpers/paths.js';
+// Importando funciones del controlador
+import { getAddProduct, postAddProduct } from '../controllers/products.controller.js'
 
 // Creando una instancia del enrutador de express
 const router = Router();
 
-// Creando datos en la memoria volatil
-export const products = [];
+// GET /admin/add-product
+router.get('/add-product', getAddProduct);
 
-// GET /add-product
-router.get('/admin/add-product', (req, res, next) => {
-  // Servimos el formulario
-  console.log("📢 Sirviendo formulario...");
-  res.render('add-product', {
-    addProduct: `active`, 
-    viewStyle: '/css/product.css',
-    viewStyle: '/css/add-product.css',
-    docTitle:"Add Product"});
-});
-// POST /add-product
-router.post('/admin/add-product', (req, res) => {
-     // Realizaremos la desestructuracion de
-  // "name" de la petición
-  const { title } = req.body;
-  // Agregamos el dato en la base de datos
-  products.push(title);
-    res.redirect('/');
-  });
+// POST /admin/add-product
+router.post('/add-product', postAddProduct);
+
   // Exportando el enrutador admin
 export default router;
